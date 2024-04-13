ca791bfaede03cadd062a57b8bb719ff02576042
MohammedAlshami <alshamicourses@gmail.com>
2024-04-14 00:37:57 +0800

    added langchain

--- app.py
+++ app.py
@@ -84,6 +84,3 @@ def hello_world():
     
     # Convert the dictionary to JSON and return
     return jsonify(response)
-# Run the Flask app
-if __name__ == "__main__":
-    app.run(debug=True)  # Set debug=True for development
