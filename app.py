71a912ed08d677e1321a6899ec6bd00cf447c552
MohammedAlshami <alshamicourses@gmail.com>
2024-04-13 20:33:18 +0800

    a

--- app.py
+++ app.py
@@ -5,6 +5,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    data = {"date": "2024-04-13"}
+    data = {
+        "choices": [
+            {
+                "finish_reason": "stop",
+                "index": 0,
+                "message": {
+                    "content": "The 2020 World Series was played in Texas at Globe Life Field in Arlington.",
+                    "role": "assistant"
+                },
+                "logprobs": None
+            }
+        ],
+        "created": 1677664795,
+        "id": "chatcmpl-7QyqpwdfhqwajicIEznoc6Q47XAyW",
+        "model": "gpt-3.5-turbo-0613",
+        "object": "chat.completion",
+        "usage": {
+            "completion_tokens": 17,
+            "prompt_tokens": 57,
+            "total_tokens": 74
+        }
+    }
     # Convert the dictionary to JSON and return
     return jsonify(data)
